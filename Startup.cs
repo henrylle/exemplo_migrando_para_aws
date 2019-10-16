72b01efe56b3b6fcb6bdaf5b042a64bd9976ea29
Henrylle Maia <henryllemaia@gmail.com>
2019-10-16 18:38:36 -0300

    Migrando para dotnetcore 3.0

--- Startup.cs
+++ Startup.cs
@@ -6,9 +6,9 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace migrando_para_aws
 {
@@ -30,29 +30,26 @@ namespace migrando_para_aws
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
-
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)        
+        {                       
+            if(env.IsDevelopment()){
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
+            else{
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints=>{
+                endpoints.MapRazorPages();
+            });
             app.UseStaticFiles();
-            app.UseCookiePolicy();
-
-            app.UseMvc();
+            app.UseCookiePolicy();            
         }
     }
 }
