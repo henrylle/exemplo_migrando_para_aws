64fb1d22703382f264ff99b7c60b7fe4a617ef77
Henrylle Maia <henryllemaia@gmail.com>
2022-02-10 17:03:31 -0300

    atualizando texto para versao 6

--- Program.cs
+++ Program.cs
@@ -10,15 +10,15 @@ using Microsoft.Extensions.Logging;
 
 namespace migrando_para_aws
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+      CreateWebHostBuilder(args).Build().Run();
     }
+
+    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>();
+  }
 }
